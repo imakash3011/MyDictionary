007c2436e0cabe30e9e0c9f424d7d4a3c15b54f4
imakash3011 <imakashpatel3011@gmail.com>
2020-05-25 10:27:34 +0530

    search capital and title word form

--- main.py
+++ main.py
@@ -6,7 +6,19 @@ def translate(word):
     word=word.lower()  #changing all word to lower case
     if word in data:
         return data[word]
-            
+    elif word.title() in data:   #changing tamil into Tamil so that both can work
+        return data[word.title()]
+    elif word.upper() in data:   #changing tamil into TAMIL so that both can work
+        return data[word.upper()]
+    elif len(get_close_matches(word,data.keys()))>0:     #("abc",["abcd","abcde","acbed"]) like this
+        print("did you mean %s instead " %get_close_matches(word,data.keys())[0])
+        decide=input("press y for yes and n for no ")
+        if decide=="y":
+            return data[get_close_matches(word,data.keys())[0]]
+        elif decide=="n":
+            return("Sorry no match found ")
+        else:
+            return("you have entered wrong input ")        
     else:
         print("Sorry no match found ")
     
